217ab0d0441a6e42e2c998e8f3eada9cd7e47fa6
hexahedron <86612194+gloomn@users.noreply.github.com>
2021-07-01 00:40:14 +0900

    basic layout
    Finished basic layout except maximize/minimize with dragging form and split window.

--- WeatherApp/WeatherApp/main.Designer.cs
+++ WeatherApp/WeatherApp/main.Designer.cs
@@ -31,23 +31,27 @@ namespace WeatherApp
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
-            this.elipse = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.navBar = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.minimizeButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.exitButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.navBarTitle = new System.Windows.Forms.Label();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.maximizeButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.smallerButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.mainFormShadow = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
             this.navBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // elipse
-            // 
-            this.elipse.TargetControl = this;
-            // 
             // navBar
             // 
             this.navBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
-            this.navBar.Controls.Add(this.label1);
+            this.navBar.Controls.Add(this.minimizeButton);
+            this.navBar.Controls.Add(this.exitButton);
+            this.navBar.Controls.Add(this.navBarTitle);
             this.navBar.Controls.Add(this.guna2PictureBox1);
+            this.navBar.Controls.Add(this.maximizeButton);
+            this.navBar.Controls.Add(this.smallerButton);
             this.navBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.navBar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
             this.navBar.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
@@ -55,9 +59,77 @@ namespace WeatherApp
             this.navBar.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
             this.navBar.Location = new System.Drawing.Point(0, 0);
             this.navBar.Name = "navBar";
+            this.navBar.ShadowDecoration.BorderRadius = 10;
             this.navBar.ShadowDecoration.Parent = this.navBar;
             this.navBar.Size = new System.Drawing.Size(960, 25);
             this.navBar.TabIndex = 0;
+            this.navBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.navBar_MouseDown);
+            this.navBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.navBar_MouseMove);
+            this.navBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.navBar_MouseUp);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeButton.CheckedState.Parent = this.minimizeButton;
+            this.minimizeButton.CustomImages.Parent = this.minimizeButton;
+            this.minimizeButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.minimizeButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.minimizeButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.minimizeButton.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.minimizeButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.minimizeButton.DisabledState.Parent = this.minimizeButton;
+            this.minimizeButton.FillColor = System.Drawing.Color.Transparent;
+            this.minimizeButton.FillColor2 = System.Drawing.Color.Transparent;
+            this.minimizeButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.minimizeButton.ForeColor = System.Drawing.Color.White;
+            this.minimizeButton.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.minimizeButton.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.minimizeButton.HoverState.Parent = this.minimizeButton;
+            this.minimizeButton.Image = ((System.Drawing.Image)(resources.GetObject("minimizeButton.Image")));
+            this.minimizeButton.ImageSize = new System.Drawing.Size(25, 25);
+            this.minimizeButton.Location = new System.Drawing.Point(840, 0);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.ShadowDecoration.Parent = this.minimizeButton;
+            this.minimizeButton.Size = new System.Drawing.Size(40, 25);
+            this.minimizeButton.TabIndex = 3;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.CheckedState.Parent = this.exitButton;
+            this.exitButton.CustomImages.Parent = this.exitButton;
+            this.exitButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.exitButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.exitButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.exitButton.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.exitButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.exitButton.DisabledState.Parent = this.exitButton;
+            this.exitButton.FillColor = System.Drawing.Color.Transparent;
+            this.exitButton.FillColor2 = System.Drawing.Color.Transparent;
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.exitButton.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.exitButton.HoverState.Parent = this.exitButton;
+            this.exitButton.Image = ((System.Drawing.Image)(resources.GetObject("exitButton.Image")));
+            this.exitButton.Location = new System.Drawing.Point(920, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.ShadowDecoration.Parent = this.exitButton;
+            this.exitButton.Size = new System.Drawing.Size(40, 25);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // navBarTitle
+            // 
+            this.navBarTitle.AutoSize = true;
+            this.navBarTitle.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.navBarTitle.ForeColor = System.Drawing.Color.White;
+            this.navBarTitle.Location = new System.Drawing.Point(24, 3);
+            this.navBarTitle.Name = "navBarTitle";
+            this.navBarTitle.Size = new System.Drawing.Size(93, 19);
+            this.navBarTitle.TabIndex = 1;
+            this.navBarTitle.Text = "Weather App";
             // 
             // guna2PictureBox1
             // 
@@ -71,16 +143,63 @@ namespace WeatherApp
             this.guna2PictureBox1.TabIndex = 1;
             this.guna2PictureBox1.TabStop = false;
             // 
-            // label1
+            // maximizeButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(24, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Weather App";
+            this.maximizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.maximizeButton.CheckedState.Parent = this.maximizeButton;
+            this.maximizeButton.CustomImages.Parent = this.maximizeButton;
+            this.maximizeButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.maximizeButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.maximizeButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.maximizeButton.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.maximizeButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.maximizeButton.DisabledState.Parent = this.maximizeButton;
+            this.maximizeButton.FillColor = System.Drawing.Color.Transparent;
+            this.maximizeButton.FillColor2 = System.Drawing.Color.Transparent;
+            this.maximizeButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.maximizeButton.ForeColor = System.Drawing.Color.White;
+            this.maximizeButton.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.maximizeButton.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.maximizeButton.HoverState.Parent = this.maximizeButton;
+            this.maximizeButton.Image = ((System.Drawing.Image)(resources.GetObject("maximizeButton.Image")));
+            this.maximizeButton.ImageSize = new System.Drawing.Size(15, 15);
+            this.maximizeButton.Location = new System.Drawing.Point(880, 0);
+            this.maximizeButton.Name = "maximizeButton";
+            this.maximizeButton.ShadowDecoration.Parent = this.maximizeButton;
+            this.maximizeButton.Size = new System.Drawing.Size(40, 25);
+            this.maximizeButton.TabIndex = 2;
+            this.maximizeButton.Click += new System.EventHandler(this.maximizeButton_Click);
+            // 
+            // smallerButton
+            // 
+            this.smallerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.smallerButton.CheckedState.Parent = this.smallerButton;
+            this.smallerButton.CustomImages.Parent = this.smallerButton;
+            this.smallerButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.smallerButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.smallerButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.smallerButton.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.smallerButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.smallerButton.DisabledState.Parent = this.smallerButton;
+            this.smallerButton.FillColor = System.Drawing.Color.Transparent;
+            this.smallerButton.FillColor2 = System.Drawing.Color.Transparent;
+            this.smallerButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.smallerButton.ForeColor = System.Drawing.Color.White;
+            this.smallerButton.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.smallerButton.HoverState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.smallerButton.HoverState.Parent = this.smallerButton;
+            this.smallerButton.Image = ((System.Drawing.Image)(resources.GetObject("smallerButton.Image")));
+            this.smallerButton.ImageSize = new System.Drawing.Size(25, 25);
+            this.smallerButton.Location = new System.Drawing.Point(880, 0);
+            this.smallerButton.Name = "smallerButton";
+            this.smallerButton.ShadowDecoration.Parent = this.smallerButton;
+            this.smallerButton.Size = new System.Drawing.Size(40, 25);
+            this.smallerButton.TabIndex = 4;
+            this.smallerButton.Click += new System.EventHandler(this.smallerButton_Click);
+            // 
+            // mainFormShadow
+            // 
+            this.mainFormShadow.TargetForm = this;
             // 
             // main
             // 
@@ -94,6 +213,7 @@ namespace WeatherApp
             this.Name = "main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Weather App";
+            this.Load += new System.EventHandler(this.main_Load);
             this.navBar.ResumeLayout(false);
             this.navBar.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
@@ -102,11 +222,14 @@ namespace WeatherApp
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2Elipse elipse;
         private Guna.UI2.WinForms.Guna2CustomGradientPanel navBar;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label navBarTitle;
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2ShadowForm mainFormShadow;
+        private Guna.UI2.WinForms.Guna2GradientButton exitButton;
+        private Guna.UI2.WinForms.Guna2GradientButton minimizeButton;
+        private Guna.UI2.WinForms.Guna2GradientButton maximizeButton;
+        private Guna.UI2.WinForms.Guna2GradientButton smallerButton;
     }
 }
 
--- WeatherApp/WeatherApp/main.cs
+++ WeatherApp/WeatherApp/main.cs
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -20,10 +21,85 @@ namespace WeatherApp
 {
     public partial class main : Form
     {
+        int mov;
+        int movX;
+        int movY;
+        private const int cGrip = 16;
+        private const int cCaption = 32;
+
         public main()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.MinimumSize = new Size(800, 450);
         }
 
+        public void navBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        public void navBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        public void navBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if(m.Msg == 0x84)
+            {
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if(pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+                if(pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void maximizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            smallerButton.BringToFront();
+        }
+
+        private void smallerButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            maximizeButton.BringToFront();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
