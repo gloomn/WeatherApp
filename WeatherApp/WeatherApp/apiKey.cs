a17a53d7be5f047f497812c51c72c8abfd753a0e
gloomn <86612194+gloomn@users.noreply.github.com>
2021-07-12 00:08:30 +0900

    Update apiKey.cs

--- WeatherApp/WeatherApp/apiKey.cs
+++ WeatherApp/WeatherApp/apiKey.cs
@@ -11,8 +11,9 @@ namespace WeatherApp
         public static class Confidentials
         {
            
-            public const string sunStatusApiKey = "Api Key";
+            public const string sunStatusApiKey = "Api Key"; //moonStatusApiKey와 동일한 API 키입니다. 프로그램 작성시 헷갈리지 않게 둘이 나눠 작성한 것입니다.
             public const string weatherApiKey = "Api Key";
+            public const string moonStatusApiKey = "Api Key";
         }
 
     }
